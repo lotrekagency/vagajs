0f72527d273dd612a844498716d2057a17268784
andrea morosi <andreamorosi94@gmail.com>
2022-02-02 15:25:29 +0100

    Mocking progress

--- src/js/animations.js
+++ src/js/animations.js
@@ -3,3 +3,11 @@ export const optionsFade = {
   rootMargin: '0px',
   targets: document.querySelectorAll("[class*='fade-'"),
 };
+
+/*
+  export const optionsFade = {
+    root: null,
+    rootMargin: '0px',
+    targets: document.querySelectorAll("[class*='vaga-'"),
+  };
+*/
--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,15 @@ import styles from './../scss/main.scss';
 export function start (threshold = 0.25) {
   observingScroll(optionsFade,threshold);
 }
+
+/*
+  export function start (threshold = 0.25, oggettoOpzioniNonObbligatorio) {
+    if(oggettoOpzioniNonObbligatorio |== null) {
+      opzioni = oggettoOpzioniNonObbligatorio
+    } else {
+      opzioni = optionsFade
+    }
+    
+    observingScroll(optionsFade,threshold);
+  }
+*/
