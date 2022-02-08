42caa1411f006fd32319757c7c0410acabbdf1bc
andrea morosi <andreamorosi94@gmail.com>
2022-02-08 12:21:57 +0100

    Fixes

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,16 @@ import styles from './../scss/vaga.scss';
 
 export function start (customOptions) {
 
-  if(customOptions !== undefined) {
-    const finalOptions = Object.assign(options, customOptions);
-    observingScroll(finalOptions);
-  } else {
-    observingScroll(options);
-  }
-
   window.addEventListener('load', function () {
     if(document.querySelector('.vaga-block')) {
       document.querySelector('.vaga-block').classList.remove('vaga-block')
+    }
+
+    if(customOptions !== undefined) {
+      const finalOptions = Object.assign(options, customOptions);
+      observingScroll(finalOptions);
+    } else {
+      observingScroll(options);
     }
   })
 }
